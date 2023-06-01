b66b72d2605d929798651e201d6890abce739a84
Леонід Українець <bookovina@gmail.com>
2023-06-01 21:14:01 +0300

    Add project files.

---
+++ ConsoleApplication4inches.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Обчислення сантиметрів" << endl;
+    const double koef = 2.54;
+    double inches;
+    double santimetres;
+    cout << "Введіть розмір в дюймах" << endl;
+    cin >> inches;
+    santimetres = inches * koef;
+    cout << "Це буде " << santimetres << " сантиметрів." << endl;
+    return 0;
+}
